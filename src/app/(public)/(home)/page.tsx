aa8c14998c7aa81d211b615a87ac34ab7fc4df6a
Mormito <guimormis@gmail.com>
2026-01-13 13:38:08 -0300

    fix: create a dashboard component

--- src/app/(public)/(home)/page.tsx
+++ src/app/(public)/(home)/page.tsx
@@ -1,55 +1,14 @@
 "use client";
 
+import Dashboard from "@/components/dashboard";
 import Header from "@/components/header";
-import CreateForm from "@/components/dataForm";
-import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
-import { trpc } from "@/lib/trpc";
-import { Eraser, PencilLine } from "lucide-react";
-import EraseData from "@/components/eraseData";
-import DataFormModal from "@/components/dataFormModal";
 
-export default function TesteTRPC() {
-  const { data, isLoading, error } = trpc.password.passwordFindAll.useQuery();
-  
-  if (isLoading) return <p>Loading...</p>;
-  if (error) return <p>Erro: {error.message}</p>;
-
+export default function Home(){
   return (
-    <div className="w-full h-full">
+    <>
+    <Header />
+    <Dashboard />
+    </>
 
-      <Header />
-      <div className="w-full h-full px-100 py-60">
-      <Table className="w-full h-full">
-        <TableHeader>
-          <TableRow>
-            <TableHead className="text-center">ID</TableHead>
-            <TableHead>Site</TableHead>
-            <TableHead>Usuário</TableHead>
-            <TableHead>Senha</TableHead>
-            <TableHead>Ações</TableHead>
-          </TableRow>
-        </TableHeader>
-
-        <TableBody>
-          {data?.map((item) => (
-            <TableRow key={item.id}>
-              <TableCell className="text-center">{item.id}</TableCell>
-              <TableCell>{item.site}</TableCell>
-              <TableCell>{item.user}</TableCell>
-              <TableCell>{item.password}</TableCell>
-              <TableCell>
-                <div className="flex flex-row gap-2">
-                  <DataFormModal data={item} />
-                  <EraseData id={item.id}/>
-                </div>
-              </TableCell>
-            
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-      </div>
-
-    </div>
   );
 }
---
+++ src/components/dashboard.tsx
@@ -0,0 +1,52 @@
+"use client";
+
+import Header from "@/components/header";
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
+import { trpc } from "@/lib/trpc";
+import EraseData from "@/components/eraseData";
+import DataFormModal from "@/components/dataFormModal";
+
+export default function Dashboard() {
+  const { data, isLoading, error } = trpc.password.passwordFindAll.useQuery();
+  
+  if (isLoading) return <p>Loading...</p>;
+  if (error) return <p>Erro: {error.message}</p>;
+
+  return (
+    <div className="w-full h-full">
+
+      <div className="w-full h-full px-100 py-60">
+      <Table className="w-full h-full">
+        <TableHeader>
+          <TableRow>
+            <TableHead className="text-center">ID</TableHead>
+            <TableHead>Site</TableHead>
+            <TableHead>Usuário</TableHead>
+            <TableHead>Senha</TableHead>
+            <TableHead>Ações</TableHead>
+          </TableRow>
+        </TableHeader>
+
+        <TableBody>
+          {data?.map((item) => (
+            <TableRow key={item.id}>
+              <TableCell className="text-center">{item.id}</TableCell>
+              <TableCell>{item.site}</TableCell>
+              <TableCell>{item.user}</TableCell>
+              <TableCell>{item.password}</TableCell>
+              <TableCell>
+                <div className="flex flex-row gap-2">
+                  <DataFormModal data={item} />
+                  <EraseData id={item.id}/>
+                </div>
+              </TableCell>
+            
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+      </div>
+
+    </div>
+  );
+}
