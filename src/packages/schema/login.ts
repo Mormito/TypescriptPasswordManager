944e762d684631e96e3c367567a7a81921558d7e
Mormito <guimormis@gmail.com>
2026-01-15 09:21:43 -0300

    add: login schema

---
+++ src/packages/schema/login.ts
@@ -0,0 +1,10 @@
+import * as z from "zod";
+ 
+const Login = z.object({
+    email: z
+    .string()
+    .min(10, "Minimo de 10 caracteres")
+    .max(254, "Máximo de 254 caracteres"),
+
+    passwordHash: z.string(),
+});
