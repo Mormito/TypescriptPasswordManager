11d064418309013ff9f5078bf736eddbf877376f
Mormito <guimormis@gmail.com>
2026-01-20 15:05:35 -0300

    changes: Logout is a component

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,24 +1,12 @@
-import { LogOutIcon, Plus } from "lucide-react";
 import DataFormModal from "./dataFormModal";
-import { Button } from "./ui/button";
-import { useRouter } from "next/navigation";
-import { toast } from "sonner";
+import Logout from "./logout";
 
 
 export default function Header(){
-    const router = useRouter();
-
-    async function logout(){
-        await fetch("/api/logout", {
-          method: "POST",
-        });
-        toast.success('Logout realizado com êxito');
-        router.push("/");
-    }
 
     return (
         <div className="w-full flex flex-row justify-center items-center gap-8 lg:gap-10 p-5 border-b-2">
-            <Button onClick={() => logout()}><LogOutIcon /><>Logout</></Button>
+            <Logout />
             <DataFormModal />
         </div>
     );
---
+++ src/components/logout.tsx
@@ -0,0 +1,21 @@
+'use client';
+import { useRouter } from "next/navigation";
+import { toast } from "sonner";
+import { Button } from "./ui/button";
+import { LogOutIcon } from "lucide-react";
+
+export default function Logout(){
+    const router = useRouter();
+
+    async function logout(){
+        await fetch("/api/logout", {
+          method: "POST",
+        });
+        toast.success('Logout realizado com êxito');
+        router.push("/");
+    }
+
+    return (
+        <Button onClick={() => logout()}><LogOutIcon /><>Logout</></Button>
+    );
+}
